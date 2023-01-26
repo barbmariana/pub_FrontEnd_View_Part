bc308c6f713ad7e98a3061edb16cf02b9b446115
Mariana Barbosa de Souza <barbmariana@users.noreply.github.com>
2023-01-25 23:44:37 -0300

    inserindoNovasPartesHome

--- View/my-app-react/src/Telas/Home/Home.jsx
+++ View/my-app-react/src/Telas/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import CarrosselHome from '../../componentes/CarrosselHome/CarrosselHome';
+import ListaRotulos from '../../componentes/ListaRotulos/ListaRotulos';
+import SobreHome from '../../componentes/SobreHome/SobreHome';
 
 
 class Home extends React.Component {
@@ -7,7 +9,11 @@ class Home extends React.Component {
 
     render() {
         return (
-              <CarrosselHome text-danger={['Pub Blinders', 'Produtos', 'Eventos']}></CarrosselHome>
+              <div>
+                <CarrosselHome text-danger={['Pub Blinders', 'Produtos', 'Eventos']}></CarrosselHome>
+                <SobreHome></SobreHome>
+                <ListaRotulos></ListaRotulos>
+              </div>
         )
     }
 }
