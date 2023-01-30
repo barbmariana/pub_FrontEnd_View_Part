b4bdf3c9e555e109bbdb7aa28910289c9a89e184
Mariana Barbosa de Souza <barbmariana@users.noreply.github.com>
2023-01-29 22:59:26 -0300

    terminandoEventos

--- View/my-app-react/src/Telas/Eventos/Evento.jsx
+++ View/my-app-react/src/Telas/Eventos/Evento.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import BannerEventos from '../../componentes/BannerEventos/BannerEventos';
+import EventosPagina from '../../componentes/EventosPagina/EventosPagina';
+import style from '../Eventos/eventos.module.css'
+
 
 class Eventos extends React.Component {
 
-
     render() {
         return (
-            <div>
-                Eventos PAGE
-            </div>
+              <div>
+                <BannerEventos></BannerEventos>
+                <h1 className={style.titulo}>Acompanhe os eventos do mês</h1>
+                <EventosPagina></EventosPagina>
+              </div>
         )
     }
 }
